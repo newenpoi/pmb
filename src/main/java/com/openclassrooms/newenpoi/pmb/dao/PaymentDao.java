e94a2be0cd263fd0fa2745d23336360e565dd96c
newenpoi <lammenschristopher119@hotmail.com>
2023-04-30 15:06:03 +0200

    dao methods

--- src/main/java/com/openclassrooms/newenpoi/pmb/dao/PaymentDao.java
+++ src/main/java/com/openclassrooms/newenpoi/pmb/dao/PaymentDao.java
@@ -1,9 +1,11 @@
 package com.openclassrooms.newenpoi.pmb.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.openclassrooms.newenpoi.pmb.business.Payment;
 
 public interface PaymentDao extends JpaRepository<Payment, Long> {
-
+	Page<Payment> findAll(Pageable pageable);
 }
--- src/main/java/com/openclassrooms/newenpoi/pmb/dao/UserDao.java
+++ src/main/java/com/openclassrooms/newenpoi/pmb/dao/UserDao.java
@@ -1,9 +1,14 @@
 package com.openclassrooms.newenpoi.pmb.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.openclassrooms.newenpoi.pmb.business.User;
 
 public interface UserDao extends JpaRepository<User, Long> {
-
+	@Query("SELECT c FROM User u JOIN u.contacts c WHERE u.id = :idUser")
+	List<User> findUserContactsById(@Param("idUser") Long idUser);
 }
