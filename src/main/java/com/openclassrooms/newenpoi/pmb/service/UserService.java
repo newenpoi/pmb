2b053a9f7e6b75d41124ac9ff6ee5609abe24115
newenpoi <lammenschristopher119@hotmail.com>
2023-05-18 13:31:52 +0200

    contact add and auth object

--- src/main/java/com/openclassrooms/newenpoi/pmb/service/UserService.java
+++ src/main/java/com/openclassrooms/newenpoi/pmb/service/UserService.java
@@ -6,14 +6,18 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import com.openclassrooms.newenpoi.pmb.business.User;
+import com.openclassrooms.newenpoi.pmb.dto.UserDTO;
 import com.openclassrooms.newenpoi.pmb.dto.UserForm;
 
 public interface UserService {
 	List<User> recupererContacts(Long idUser);
 	Page<User> recupererPageContacts(Long idUser, Pageable pageable);
 	
-	User ajouterContact(Long idUser, String email);
+	User ajouterContact(String emailUser, String emailContact);
 	User register(UserForm userForm);
 	User modifierProfil(Long idUser, UserForm userForm);
+	
 	User recupererUtilisateur(Long idUser);
+	User recupererUtilisateur(String email);
+	UserDTO recupererUtilisateurDTO(String email);
 }
